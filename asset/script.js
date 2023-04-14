777b7f3e86f8e67e82b7fa9ce39948777a697a93
ADITYA <adityapuri85@gmail.com>
2023-04-14 18:18:16 +0530

    added a contact popup

--- asset/script.js
+++ asset/script.js
@@ -4,27 +4,25 @@ console.log("hi");
 var bars = document.getElementsByClassName("bars")[0];
 var toggleElm = document.getElementsByClassName("toggle")[0];
 
-console.log(bars);
-// bars.map((bar) => {
 bars.onclick = () => {
   console.log(bars);
   console.log(toggleElm);
 
   toggleElm.classList.toggle("toggle-on");
 };
-// });
 
 // ......................................................
 
-// Pop up--MODAL
+// Pop up--MODAL -subscribe
 /// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.querySelectorAll("#myBtn");
-console.log(btn);
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelectorAll(".close");
+
+console.log(span);
 
 // When the user clicks the button, open the modal
 btn.forEach((btn) => {
@@ -33,13 +31,37 @@ btn.forEach((btn) => {
   };
 });
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+span.forEach((span) => {
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+};
+
+//  Pop up--MODAL -contact popup
+var contactModal = document.getElementById("contactModal");
+
+var btn = document.querySelector("#contactUsBtn");
+console.log(btn);
+
+btn.onclick = function () {
+  contactModal.style.display = "block";
+};
+
+span.forEach((span) => {
+  span.onclick = function () {
+    contactModal.style.display = "none";
+  };
+});
+
+window.onclick = function (event) {
+  if (event.target == contactModal) {
+    contactModal.style.display = "none";
   }
 };
