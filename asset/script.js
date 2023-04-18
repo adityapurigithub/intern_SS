685c219bc7124b0fce08454db536609109e5f539
ADITYA <adityapuri85@gmail.com>
2023-04-18 13:23:36 +0530

    added-- event count down timer

--- asset/script.js
+++ asset/script.js
@@ -50,16 +50,52 @@ var btn = document.querySelector("#contactUsBtn");
 console.log(btn);
 
 var close = document.querySelector("#closeContact");
-btn.onclick = function () {
-  contactModal.style.display = "block";
-};
+if (btn) {
+  btn.onclick = function () {
+    contactModal.style.display = "block";
+  };
+}
 
-close.onclick = function () {
-  contactModal.style.display = "none";
-};
+if (close) {
+  close.onclick = function () {
+    contactModal.style.display = "none";
+  };
+}
 
 window.onclick = function (event) {
   if (event.target == contactModal) {
     contactModal.style.display = "none";
   }
 };
+
+//count down timer...........................................
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jul 5, 2023 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  console.log(distance);
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementsByClassName("timer")[0].innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// ......................
